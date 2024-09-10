77a29bd3e3c18d55c01fd84fcd0ae851c23d3807
Toha <tohenk@yahoo.com>
2024-09-11 01:42:06 +0700

    Do not ignore when extracting from archive.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- scripts/sync-src.sh
+++ scripts/sync-src.sh
@@ -3,11 +3,15 @@
 LOG=/var/log/sync-src.log
 
 SRC_DIR=/src
-if [ -n "${TARGET_DIR}" -a -n "${EXCLUDE_FILE}" ]; then
+if [ -n "${TARGET_DIR}" ]; then
   mkdir -p ${TARGET_DIR}
   if [ -f $SRC_DIR/dist/app.tgz ]; then
-    tar -xvzf $SRC_DIR/dist/app.tgz --exclude-from=${EXCLUDE_FILE} -C ${TARGET_DIR}>>$LOG
+    tar -xvzf $SRC_DIR/dist/app.tgz -C ${TARGET_DIR}>>$LOG
   else
-    rsync -avzi --exclude-from=${EXCLUDE_FILE} ${SRC_DIR} ${TARGET_DIR}>>$LOG
+    if [ -n "${EXCLUDE_FILE}" ]; then
+      rsync -avzi --exclude-from=${EXCLUDE_FILE} ${SRC_DIR} ${TARGET_DIR}>>$LOG
+    else
+      rsync -avzi ${SRC_DIR} ${TARGET_DIR}>>$LOG
+    fi
   fi
 fi

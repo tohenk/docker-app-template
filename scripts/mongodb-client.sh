7db82c911c6e49254e91cec0011e31c513cff2bb
Toha <tohenk@yahoo.com>
2024-09-19 13:27:01 +0700

    Update MongoDB to 7.0.14 and fix APT repository.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- scripts/mongodb-client.sh
+++ scripts/mongodb-client.sh
@@ -7,8 +7,8 @@ apt-get install $APT_OPTS -y curl gnupg>/dev/null
 # setup mongodb repository, see https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-debian/
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
   gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main" | \
-  tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-
+cat <<EOF > /etc/apt/sources.list.d/mongodb-org-7.0.list
+deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main
+EOF
 apt-get update $APT_OPTS>/dev/null
 apt-get install $APT_OPTS -y mongodb-org-tools mongodb-mongosh>/dev/null

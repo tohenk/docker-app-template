34004b00303c9cc797ac0871abebfabd330c9f2c
Toha <tohenk@yahoo.com>
2024-11-25 19:11:48 +0700

    Support additional APT packages via APT_PACKAGES.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -2,13 +2,9 @@
 
 LOG=/var/log/apt.log
 
-apt_mirror() {
-  [ -f /etc/apt/sources.list.d/debian.sources ] && sed -i -e "s/deb.debian.org/${APT_MIRROR}/g" /etc/apt/sources.list.d/debian.sources
-}
-
-apt_updates() {
-  apt-get update>>$LOG
-}
-
-apt_mirror
-apt_updates
+[ -f /etc/apt/sources.list.d/debian.sources ] && \
+  sed -i -e "s/deb.debian.org/${APT_MIRROR}/g" /etc/apt/sources.list.d/debian.sources
+apt-get update>>$LOG
+if [ -n "${APT_PACKAGES}" ]; then
+  apt-get install -y ${APT_PACKAGES}>>$LOG
+fi

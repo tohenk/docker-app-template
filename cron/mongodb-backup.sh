92bc3a84e08cfd4955ff3967c5c0c509ce066318
Toha <tohenk@yahoo.com>
2024-12-02 18:50:34 +0700

    Backup all databases by default.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- cron/mongodb-backup.sh
+++ cron/mongodb-backup.sh
@@ -10,53 +10,79 @@ ENV=${CD}/mongodb-backup.env
 
   MONGODUMP=$(which mongodump)
   if [ -n "$MONGODUMP" -a -n "${DB_BACKUPS}" ]; then
+    QUERY=/tmp/query.json
+    SCRIPT=/tmp/mongodb-backup.js
+    # list databases to backup if wildcard
+    if [ "$DB_BACKUPS" = "*" ]; then
+      cat << EOF > $SCRIPT
+res = db.adminCommand({listDatabases: 1});
+dbs = res.databases.map(d => d.name);
+print(dbs.join(' '));
+EOF
+      DB_BACKUPS=$(mongosh -u $DB_USER -p $DB_PASSWORD --authenticationDatabase=admin --quiet mongodb://$DB_HOST:$DB_PORT/admin $SCRIPT)
+    fi
     # prepare backup storage
     TOPDIR=/backup/MongoDB
-    BACKUP="delta"
-    if [ -d $TOPDIR/full ]; then
-      DIRS=$(ls $TOPDIR/full)
-      [ -z "$DIRS" ] && BACKUP="full"
-    else
-      BACKUP="full"
-    fi
-    BACKUPDIR=$TOPDIR/$BACKUP/$(date +%Y%m%d)
-    # get backup last date
-    LASTDATE=""
-    if [ "$BACKUP" != "full" ]; then
-      # get last date from delta backup
-      XDIR=$(dirname $BACKUPDIR)
-      DIRS=$(ls $XDIR)
-      if [ -n "$DIRS" ]; then
-        echo "Found backup entries [$DIRS]..."
-        for DIR in $DIRS; do
-          LASTDATE=$DIR
-        done
-      fi
-      # get last date from full backup
-      if [ -z "$LASTDATE" ]; then
+    for DB in $DB_BACKUPS; do
+      echo "--- mongodb://$DB_HOST:$DB_PORT/$DB ---"
+      # get backup type -> full or delta
+      BACKUP="delta"
+      if [ -d "$TOPDIR/full" ]; then
+        BACKUP="full"
         DIRS=$(ls $TOPDIR/full)
         if [ -n "$DIRS" ]; then
           for DIR in $DIRS; do
-            LASTDATE=$DIR
+            if [ -d "$TOPDIR/full/$DIR/$DB" ]; then
+              BACKUP="delta"
+              break
+            fi
           done
         fi
+      else
+        BACKUP="full"
       fi
-    fi
-    # create backup directory
-    mkdir -p $BACKUPDIR
-    # execute backup
-    if [ "$BACKUP" = "full" ]; then
-      echo "Doing mongodb database full dump..."
-      $MONGODUMP --host $DB_HOST:$DB_PORT -u $DB_USER -p $DB_PASSWORD --authenticationDatabase=admin -j 1 --gzip --out=$BACKUPDIR
-    elif [ -n "$LASTDATE" ]; then
-      DATE1="${LASTDATE:0:4}-${LASTDATE:4:2}-${LASTDATE:6:2}T23:59:59.999Z"
-      DATE2="`date +%Y-%m-%d`T23:59:59.999Z"
-      echo "Last backup date $LASTDATE..."
-      echo "Delta backup performed from $DATE1 to $DATE2..."
-      for DB in $DB_BACKUPS; do
+      BACKUPDIR=$TOPDIR/$BACKUP/$(date +%Y%m%d)
+      # get backup last date
+      LASTDATE=""
+      if [ "$BACKUP" != "full" ]; then
+        # get last date from delta backup
+        XDIR=$(dirname $BACKUPDIR)
+        DIRS=$(ls $XDIR)
+        if [ -n "$DIRS" ]; then
+          echo "Found backup entries [${DIRS//$'\n'/ }]..."
+          for DIR in $DIRS; do
+            if [ -d "$XDIR/$DIR/$DB" ]; then
+              LASTDATE=$DIR
+              break
+            fi
+          done
+        fi
+        # get last date from full backup
+        if [ -z "$LASTDATE" ]; then
+          DIRS=$(ls $TOPDIR/full)
+          if [ -n "$DIRS" ]; then
+            for DIR in $DIRS; do
+              if [ -d "$TOPDIR/full/$DIR/$DB" ]; then
+                LASTDATE=$DIR
+                break
+              fi
+            done
+          fi
+        fi
+      fi
+      # create backup directory
+      mkdir -p $BACKUPDIR
+      # execute backup
+      if [ "$BACKUP" = "full" ]; then
+        echo "Creating mongodb database full dump for $DB..."
+        $MONGODUMP -h $DB_HOST:$DB_PORT -u $DB_USER -p $DB_PASSWORD --authenticationDatabase=admin -j 1 --gzip --out=$BACKUPDIR \
+          --db=$DB
+      elif [ -n "$LASTDATE" ]; then
         echo "Creating mongodb database delta dump for $DB..."
-        QUERY=/tmp/query.json
-        SCRIPT=/tmp/ids.js
+        DATE1="${LASTDATE:0:4}-${LASTDATE:4:2}-${LASTDATE:6:2}T23:59:59.999Z"
+        DATE2="`date +%Y-%m-%d`T23:59:59.999Z"
+        echo "Last backup date $LASTDATE..."
+        echo "Delta backup performed from $DATE1 to $DATE2..."
         # backup fs.files collection
         cat << EOF > $QUERY
 {"uploadDate":{"\$gt":{"\$date":"$DATE1"},"\$lte":{"\$date":"$DATE2"}}}
@@ -73,7 +99,7 @@ while (c.hasNext()) {
 }
 print(res.join(','));
 EOF
-        IDS=`mongosh -u $DB_USER -p $DB_PASSWORD --authenticationDatabase=admin --quiet mongodb://$DB_HOST:$DB_PORT/$DB $SCRIPT`
+        IDS=$(mongosh -u $DB_USER -p $DB_PASSWORD --authenticationDatabase=admin --quiet mongodb://$DB_HOST:$DB_PORT/$DB $SCRIPT)
         if [ -n "$IDS" ]; then
           cat << EOF > $QUERY
 {"files_id":{"\$in":[$IDS]}}
@@ -81,7 +107,7 @@ EOF
           $MONGODUMP -h $DB_HOST:$DB_PORT -u $DB_USER -p $DB_PASSWORD --authenticationDatabase=admin -j 1 --gzip --out=$BACKUPDIR \
             --db=$DB --collection=fs.chunks --queryFile=$QUERY
         fi
-      done
-    fi
+      fi
+    done
   fi
 }
--- cron/mysql-backup.sh
+++ cron/mysql-backup.sh
@@ -12,6 +12,10 @@ ENV=${CD}/mysql-backup.env
   if [ -n "$MYSQLDUMP" -a -n "${DB_BACKUPS}" ]; then
     ZZ=$(which 7z)
     [ -z "${ZZ}" ] && ZZ=$(which 7zz)
+    # list databases to backup if wildcard
+    if [ "$DB_BACKUPS" = "*" ]; then
+      DB_BACKUPS=$(echo "SHOW DATABASES;" | mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD --silent)
+    fi
     # prepare backup storage
     BACKUPDIR=/backup/MySQL
     if [ `date +%d` = "01" ]; then
@@ -25,6 +29,9 @@ ENV=${CD}/mysql-backup.env
     mkdir -p $BACKUPDIR
     # execute backup
     for DB in $DB_BACKUPS; do
+      if [ "$DB" = "mysql" -o "$DB" = "information_schema" -o "$DB" = "sys" ]; then
+        continue
+      fi
       DB_BACKUP=$BACKUPDIR/$DB.sql.7z
       echo "Creating MySQL database dump for $DB..."
       [ -f "$DB_BACKUP" ] && mv "$DB_BACKUP" "$DB_BACKUP~"
@@ -36,7 +43,7 @@ ENV=${CD}/mysql-backup.env
       BACKUPDIR=$(dirname $BACKUPDIR)
       DIRS=$(ls $BACKUPDIR)
       if [ -n "$DIRS" ]; then
-        echo "Found daily backup entries [$DIRS]..."
+        echo "Found daily backup entries [${DIRS//$'\n'/ }]..."
         MAXBACKUP=7
         N=0
         for DIR in $DIRS; do
